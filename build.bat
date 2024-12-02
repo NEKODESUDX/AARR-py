58a43fb5ff42ef5d24fb83967acfe6d7c1973eae
NEKODESUDX <piyo111111112@gmail.com>
2024-12-02 17:34:15 +0900

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,3 +3,7 @@ requests
 python -m pip install --user --upgrade git+https://github.com/Merubokkusu/Discord-S.C.U.M.git#egg=discum
 git+https://github.com/Merubokkusu/Discord-S.C.U.M.git#egg=discum
 time
+pip install -r requirements.txt
+pip install typing_extensions
+pip install emoji
+pip install filetype
